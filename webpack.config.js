6ba510920e4752a87c87e1bb91a8ccb2c3492940
Bogdan Bivolaru <bogdan.bivolaru@gmail.com>
2017-11-01 17:01:59 +0200

    attempt at lesson8

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+const webpack = require('webpack')
 const {resolve} = require('path')
 module.exports = env => {
+  const addPlugin = (add, plugin) => (add ? plugin : undefined)
+  const ifProd = plugin => addPlugin(env.prod, plugin)
+  const removeEmpty = array => array.filter(i => !!i)
   return {
     entry: './js/app.js',
     output: {
@@ -10,11 +14,37 @@ module.exports = env => {
     context: resolve(__dirname, 'src'),
     devtool: env.prod ? 'source-map' : 'eval',
     bail: env.prod,
+    resolveLoader: {
+      moduleExtensions: ["-loader"]
+    },
     module: {
       loaders: [
-        {test: /\.js$/, loader: 'babel!eslint', exclude: /node_modules/},
-        {test: /\.css$/, loader: 'style!css'},
+        {
+          test: /\.js$/,
+          loader: 'babel-loader!eslint',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          loader: 'style-loader!css'
+        },
       ],
     },
+    plugins: removeEmpty([
+      // ifProd(new webpack.optimize.DedupePlugin()),
+      ifProd(new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false,
+      })),
+      ifProd(new webpack.DefinePlugin({
+        NODE_ENV: '"production"'
+      })),
+      ifProd(new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          screw_ie8: true,
+          warnings: false,
+        }
+      }))
+    ]),
   }
 }
